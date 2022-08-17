43063221ce62366ef229027a13c1a6f701203b05
sozy <thinpotatogithub@gmail.com>
2022-08-17 18:09:17 +0700

    feat/lectures

--- src/services/course.service.ts
+++ src/services/course.service.ts
@@ -41,17 +41,17 @@ export class CourseService {
     return totalCourse
   }
 
-    async createCourse(payload: CourseInput) {
-        const result = CourseSchema.safeParse({
-            title: payload.title,
-            description: payload.description,
-            image: payload.image,
-        });
-        if (!result.success) {
-            return this.failedOrSuccessRequest('failed', result.error)
-        }
-        const course = await Course.create(payload)
-        return this.failedOrSuccessRequest('success', course)
+  async createCourse(payload: CourseInput) {
+    const result = CourseSchema.safeParse({
+      title: payload.title,
+      description: payload.description,
+      image: payload.image,
+    });
+    if (!result.success) {
+      return this.failedOrSuccessRequest('failed', result.error)
+    }
+    const course = await Course.create(payload)
+    return this.failedOrSuccessRequest('success', course)
 
 =======
   async createCourse(payload: CourseInput) {
@@ -63,141 +63,141 @@ export class CourseService {
     if (!result.success) {
       return this.failedOrSuccessRequest('failed', result.error)
 >>>>>>> 5d287a0 (feat/lectures)
-    }
-    const course = await Course.create(payload)
+  }
+  const course = await Course.create(payload)
     return this.failedOrSuccessRequest('success', course)
 
   }
 
   async deleteCourse(id: number) {
-    const course = await Course.destroy({
-      where: {
-        id: id
-      }
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course not found')
-    } else {
-      return this.failedOrSuccessRequest('Success Delete Course', course)
+  const course = await Course.destroy({
+    where: {
+      id: id
     }
+  })
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course not found')
+  } else {
+    return this.failedOrSuccessRequest('Success Delete Course', course)
   }
+}
 
   async getAllCourses(page: number, limit: number) {
-    const course = await Course.findAll({
-      offset: (page - 1) * limit,
-      limit: limit,
-      include: [{
-        model: Instructor,
-        as: 'instructor',
-        attributes: ['nip', 'name']
-      }],
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course not found')
-    }
-    return this.failedOrSuccessRequest('success', course)
-
+  const course = await Course.findAll({
+    offset: (page - 1) * limit,
+    limit: limit,
+    include: [{
+      model: Instructor,
+      as: 'instructor',
+      attributes: ['nip', 'name']
+    }],
+  })
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course not found')
   }
+  return this.failedOrSuccessRequest('success', course)
+
+}
 
   async getAllVerifiedCourses(page: number, limit: number) {
-    const course = await Course.findAll({
-      where: {
-        is_verified: true
-      },
-      offset: (page - 1) * limit,
-      limit: limit,
-      include: [{
-        model: Instructor,
-        as: 'instructor',
-        attributes: ['nip', 'name']
-      }],
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course not found')
-    }
-    return this.failedOrSuccessRequest('success', course)
-
+  const course = await Course.findAll({
+    where: {
+      is_verified: true
+    },
+    offset: (page - 1) * limit,
+    limit: limit,
+    include: [{
+      model: Instructor,
+      as: 'instructor',
+      attributes: ['nip', 'name']
+    }],
+  })
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course not found')
   }
+  return this.failedOrSuccessRequest('success', course)
 
-  async getBySearch(search: string, page: number, limit: number): Promise<any> {
-    const course = await Course.findAll({
-      where: {
-        is_verified: true,
-        title: {
-          [Op.iLike]: `%${search}%`
-        }
-      },
-      offset: (page - 1) * limit,
-      limit: limit,
-      include: [{
-        model: Instructor,
-        as: 'instructor',
-        attributes: ['nip','name']
-      }],
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course not found')
-    }
-    return this.failedOrSuccessRequest('success', course)
+}
+
+  async getBySearch(search: string, page: number, limit: number): Promise < any > {
+  const course = await Course.findAll({
+    where: {
+      is_verified: true,
+      title: {
+        [Op.iLike]: `%${search}%`
+      }
+    },
+    offset: (page - 1) * limit,
+    limit: limit,
+    include: [{
+      model: Instructor,
+      as: 'instructor',
+      attributes: ['nip', 'name']
+    }],
+  })
+    if(!course) {
+    return this.failedOrSuccessRequest('failed', 'Course not found')
   }
+    return this.failedOrSuccessRequest('success', course)
+}
 
   async getCourseById(id: number) {
-    const course = await Course.findByPk(id,{
-      include: [{
-        model: Instructor,
-        as: 'instructor',
-        attributes: ['nip','name']
-      }]
-    })
-    if (course == null) {
-      return this.failedOrSuccessRequest('failed', 'Course not found')
-    } else {
-      return this.failedOrSuccessRequest('success', course)
-    }
-  }
-
-  async getCourseByInstructorId(id: number): Promise<CourseOutput[]> {
-    return await Course.findAll({
-      where: {
-        instructor_id: id,
-      },
-      include: [{
-        model: Instructor,
-        as: 'instructor',
-        attributes: ['nip', 'name']
-      }],
-    })
-  }
-
-  async updateCourse(id: number, payload: CourseInput) {
-    const result = CourseSchema.safeParse({
-      title: payload.title,
-      description: payload.description,
-      image: payload.image,
-    });
-    if (!result.success) {
-      return this.failedOrSuccessRequest('failed', result.error)
-    }
-    const course = await Course.update(payload, {
-      where: {
-        id: id
-      },
-      returning: true
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course Not Found')
-    }
+  const course = await Course.findByPk(id, {
+    include: [{
+      model: Instructor,
+      as: 'instructor',
+      attributes: ['nip', 'name']
+    }]
+  })
+  if (course == null) {
+    return this.failedOrSuccessRequest('failed', 'Course not found')
+  } else {
     return this.failedOrSuccessRequest('success', course)
   }
+}
+
+  async getCourseByInstructorId(id: number): Promise < CourseOutput[] > {
+  return await Course.findAll({
+    where: {
+      instructor_id: id,
+    },
+    include: [{
+      model: Instructor,
+      as: 'instructor',
+      attributes: ['nip', 'name']
+    }],
+  })
+}
+
+  async updateCourse(id: number, payload: CourseInput) {
+  const result = CourseSchema.safeParse({
+    title: payload.title,
+    description: payload.description,
+    image: payload.image,
+  });
+  if (!result.success) {
+    return this.failedOrSuccessRequest('failed', result.error)
+  }
+  const course = await Course.update(payload, {
+    where: {
+      id: id
+    },
+    returning: true
+  })
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course Not Found')
+  }
+  return this.failedOrSuccessRequest('success', course)
+}
 
 <<<<<<< HEAD
   async verifyCourse(id: number) {
-    const course = await Course.update({
-      is_verified: true
-    }, {
-      where: {
-        id: id
-      }
+  const course = await Course.update({
+    is_verified: true
+  }, {
+    where: {
+      id: id
+    }
 =======
   async getCourseById(id: number) {
     const course = await Course.findByPk(id, {
@@ -207,35 +207,35 @@ export class CourseService {
         attributes: ['nip', 'name']
       }]
 >>>>>>> 5d287a0 (feat/lectures)
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course Not Found')
-    }
-    return this.failedOrSuccessRequest('success', course)
+  })
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course Not Found')
   }
+  return this.failedOrSuccessRequest('success', course)
+}
 
   async rejectCourse(id: number) {
-    const course = await Course.update({
-      is_verified: false
-    }, {
-      where: {
-        id: id
-      }
-    })
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course Not Found')
+  const course = await Course.update({
+    is_verified: false
+  }, {
+    where: {
+      id: id
     }
-    return this.failedOrSuccessRequest('success', course)
+  })
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course Not Found')
   }
+  return this.failedOrSuccessRequest('success', course)
+}
 
   async enrollCourse(id: number, userId: number) {
-    const course = await Course.findByPk(id)
-    if (!course) {
-      return this.failedOrSuccessRequest('failed', 'Course Not Found')
-    }
-    // const enroll = await course.addUser(userId)
-    // return this.failedOrSuccessRequest('success', enroll)
+  const course = await Course.findByPk(id)
+  if (!course) {
+    return this.failedOrSuccessRequest('failed', 'Course Not Found')
   }
+  // const enroll = await course.addUser(userId)
+  // return this.failedOrSuccessRequest('success', enroll)
+}
 
 
 
