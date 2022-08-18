c235ea11059fd20f965d057b53fe16eacf516617
sozy <thinpotatogithub@gmail.com>
2022-08-18 22:20:09 +0700

    fix: student progress

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export * from './course.service';
 export * from './user.service';
 export * from './instructor.service';
 export * from './studentQuiz.service'
+export * from './studentProgress.service'
