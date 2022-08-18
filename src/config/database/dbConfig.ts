74cd58a878339f0704001d13384047ff373f2476
fajarbh <hajargak@gmail.com>
2022-08-18 20:56:29 +0700

    fix env

--- src/config/database/dbConfig.ts
+++ src/config/database/dbConfig.ts
@@ -12,6 +12,7 @@ const dbConfig = {
     password: process.env.DB_PASSWORD ,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
     dialect: 'postgres'
   }
 }
